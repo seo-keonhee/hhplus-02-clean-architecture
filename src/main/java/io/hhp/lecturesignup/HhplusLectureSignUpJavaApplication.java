80deac281e85a654a73441525f778cc6edbd59b0
seo-keonhee <sluckhy17@gmail.com>
2024-06-25 19:34:48 +0900

    init

---
+++ src/main/java/io/hhp/lecturesignup/HhplusLectureSignUpJavaApplication.java
@@ -0,0 +1,13 @@
+package io.hhp.lecturesignup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusLectureSignUpJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusLectureSignUpJavaApplication.class, args);
+    }
+
+}
